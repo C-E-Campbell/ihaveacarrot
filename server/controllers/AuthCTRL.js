69da1f1f4227f30a8438fbc549c4902df1e52dcb
Charles <codelyfewithcharlie@gmail.com>
2020-07-17 03:44:04 -0400

    bcrypt added

--- server/controllers/AuthCTRL.js
+++ server/controllers/AuthCTRL.js
@@ -1,5 +1,5 @@
 const Users = require('../models/user');
-
+const bcrypt = require('bcrypt');
 module.exports = {
   login: async function (req, res, next) {
     const { email, user, password } = req.body;
@@ -10,25 +10,39 @@ module.exports = {
       return res.status(404).json('Create an account');
     }
 
-    return res.json({ result }).status(200);
+    bcrypt.compare(password, result.password, function (err, result) {
+      if (result) {
+        // token?
+        return res.json(result).status(200);
+      } else {
+        return res.status(666).json('Invalid username or pass');
+      }
+    });
   },
   register: async function (req, res, next) {
     const { email, user, password } = req.body;
-    const newUser = new Users({
-      user,
-      email,
-      password,
-    });
 
     // user names and emails are unique in the db - checking them here
     const checkEmail = await Users.findOne({ email });
     const checkuser = await Users.findOne({ user });
 
+    // if they dont exist - hash pass and save the user
     if (!checkEmail && !checkuser) {
-      newUser.save();
+      bcrypt.hash(password, 12, function (err, hash) {
+        const newUser = new Users({
+          user,
+          email,
+          password: hash,
+        });
+
+        newUser.save();
+      });
+
       return res.status(200).json('Account Created');
     } else {
-      return res.status(409).json('Already have an account?');
+      return res
+        .status(422)
+        .json('Email or User Name in use. Already have an account?');
     }
   },
 };
