69a6a405eae5d35d944a5a28dfddc55f3f638f38
Charles <codelyfewithcharlie@gmail.com>
2020-07-17 03:01:25 -0400

    login and register done

--- server/controllers/AuthCTRL.js
+++ server/controllers/AuthCTRL.js
@@ -4,21 +4,32 @@ module.exports = {
   login: async function (req, res, next) {
     const { email, user, password } = req.body;
 
-    const data = await Users.find();
-    console.log(data);
-    res.send('logged in');
+    const result = await Users.findOne({ email });
+
+    if (!result) {
+      res.status(404).json('Create an account');
+      return;
+    }
+
+    res.json({ result }).status(200);
+    return;
   },
   register: async function (req, res, next) {
     const { email, user, password } = req.body;
-
     const newUser = new Users({
       user,
       email,
       password,
     });
+    const checkEmail = await Users.findOne({ email });
+    const checkuser = await Users.findOne({ user });
 
-    await newUser.save();
-
-    res.send('registered');
+    if (!checkEmail && !checkuser) {
+      newUser.save();
+      res.status(200).json('Account Created');
+      return;
+    } else {
+      res.status(409).json('Already have an account?');
+    }
   },
 };
