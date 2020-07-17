a1b3bf41cc4e09cc0da4ddf3e51bdd3127432812
Charles <codelyfewithcharlie@gmail.com>
2020-07-17 02:28:37 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ require('dotenv').config({ path: '../.env' });
 const mongoose = require('mongoose');
 const express = require('express');
 const Auth = require('./routes/AuthRouter');
-const Recipes = require('./controllers/RecipesCTRL');
+const Recipes = require('./routes/RecipesRouter');
+const Users = require('./routes/UsersRouter');
 const app = express();
 const morgan = require('morgan');
 const { PORT, mongoURI } = process.env;
@@ -11,7 +12,8 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.use('/api/v1/auth', Auth);
-app.use('/api/v1/recipes', Recipes);
+// app.use('/api/v1/recipes', Recipes);
+// app.use('/api/v1/users', Users);
 
 mongoose
   .connect(mongoURI, {
